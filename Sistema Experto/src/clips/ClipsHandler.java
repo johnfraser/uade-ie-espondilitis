6598d667b37d543d6a02e3ec6846d237dc72d245
John Fraser <john.fraser.s@gmail.com>
2018-06-30 20:15:18 -0300

    CLIPS entrada y salida funciona

--- Sistema Experto/src/clips/ClipsHandler.java
+++ Sistema Experto/src/clips/ClipsHandler.java
@@ -17,176 +17,163 @@ import model.Laboratorio;
 import model.Paciente;
 
 public class ClipsHandler {
-	
+
 	private Environment env;
 	private Router router;
-	
+
 	public ClipsHandler() {
 		env = new Environment();
-		
+
 		router = new Router("router");
 		env.addRouter(router);
-		
+
 		env.watch(Environment.FACTS);
 		env.watch(Environment.RULES);
 		env.watch(Environment.STATISTICS);
 		env.watch(Environment.ACTIVATIONS);
-		
+
 		env.load("./reglasV3.clp");
 	}
-	
+
 	public Diagnostico correrConsulta(Consulta consulta) {
 		agregarPaciente(consulta.paciente);
-		
+
 		agregarDolor(consulta.dolor1);
 		agregarDolor(consulta.dolor2);
-		
+
 		agregarAntecedentesPaciente(consulta.antecedentes_paciente);
 		agregarAntecedentesFamiliares(consulta.antecedentes_familiares);
-		
+
 		agregarEstudio(consulta.estudio_gen);
 		agregarEstudio(consulta.estudio_rmn);
 		agregarEstudio(consulta.estudio_rx);
-		
+
 		agregarLaboratorio(consulta.laboratorio);
+
+		agregarDiagnostico(consulta.paciente.id_paciente, consulta.dolor1.id_diagnostico,
+				consulta.paciente.ultima_modificacion);
 		
-		agregarDiagnostico(consulta.paciente.id_paciente, consulta.dolor1.id_diagnostico, consulta.paciente.ultima_modificacion);
+		Diagnostico diag = new Diagnostico(consulta.dolor1.id_diagnostico, consulta.paciente.id_paciente);
 		
+		System.out.println("--- CLIPS START ---");
 		env.run();
-		
+		System.out.println("--- CLIPS DONE ---");
+
 		MultifieldValue pv = (MultifieldValue) env.eval("(find-all-facts((?J diagnostico))TRUE)");
-		System.out.println( pv.toString() );
 		try {
-			for(int i = 0; i < 10; i++) {
-				FactAddressValue fav =(FactAddressValue)pv.get(i);
-				System.out.println( fav.toString() );
-			}
-			
-			//FactAddressValue fav =(FactAddressValue)pv.get(0);
-			
-			//resultado = fav.getFactSlot("resultado").toString();//resultado_primera_etapa
+			FactAddressValue fav =(FactAddressValue)pv.get(0);
+
+			diag.actualizar(
+					fav.getFactSlot("dolorlumbar").toString(),
+					fav.getFactSlot("gradosospecha").toString(), 
+					fav.getFactSlot("enfermedad").toString(),
+					fav.getFactSlot("derivacion").toString(),
+					fav.getFactSlot("estudiossolicitados").toString() );
 		} catch (Exception e1) {
 			System.out.println("Failure with CLIPS output.");
 			e1.printStackTrace();
 		}
-		
-		// cargar a Diagnostico
-		// mostrar 
-		//consulta.diagnostico.actualizar(dolor_lumbar, grado_sospecha, enfermedad, derivacion, estudios_solicitados);
-		return null;
+
+		return diag;
 	}
-	
+
 	private void agregarPaciente(Paciente p) {
-		String entrada = "(paciente (id_paciente " + p.id_paciente
-				+ ") (dni " + p.dni
-				+ ") (nombre \"" + p.nombre
-				+ "\") (apellido \"" + p.apellido
-				+ "\") (sexo " + p.sexo
-				+ ") (edad " + p.edad + ") )";
-		
-		System.out.println("[Paciente]: " + entrada);
-		
+		String entrada = "(paciente (id_paciente " + p.id_paciente + ") (dni " + p.dni + ") (nombre \"" + p.nombre
+				+ "\") (apellido \"" + p.apellido + "\") (sexo " + p.sexo + ") (edad " + p.edad + ") )";
+
 		env.assertString(entrada);
 	}
-	
+
 	private void agregarDolor(Dolor d) {
-		if(d == null)
+		if (d == null)
 			return;
-		
-		// TODO: filtrar segun caso de dolor
-		String entrada = "(dolor (id_diagnostico " + d.id_diagnostico
-				+ ") (id_dolor " + d.id_dolor
-				+ ") (zona " + d.zona
-				+ ") (origen_dolor " + d.origen_dolor
-				+ ") (lodespiertanoche " + d.lodespiertanoche
-				+ ") (mejoria " + d.mejoria
-				+ ") (edad_inicio_dolor " + d.edad_inicio_dolor
-				+ ") (meses_persistencia " + d.meses_persistencia
-				+ ") (aspecto " + d.aspecto
-				+ ") (inflamacion " + d.inflamacion
-				+ ") (ritmo_evacuatorio " + d.ritmo_evacuatorio + ") )";
-		
-		System.out.println("[Dolor]: " + entrada);
-		
-		env.assertString(entrada);
+
+		StringBuilder e = new StringBuilder();
+
+		e.append("(dolor (id_diagnostico " + d.id_diagnostico);
+		e.append(") (id_dolor " + d.id_dolor);
+		if (!d.zona.isEmpty())
+			e.append(") (zona " + d.zona);
+		if (!d.origen_dolor.isEmpty())
+			e.append(") (origen_dolor " + d.origen_dolor);
+		if (!d.lodespiertanoche.isEmpty())
+			e.append(") (lodespiertanoche " + d.lodespiertanoche);
+		if (!d.mejoria.isEmpty())
+			e.append(") (mejoria " + d.mejoria);
+		if (d.edad_inicio_dolor != 0)
+			e.append(") (edad_inicio_dolor " + d.edad_inicio_dolor);
+		if (d.meses_persistencia != 0)
+			e.append(") (meses_persistencia " + d.meses_persistencia);
+		if (!d.aspecto.isEmpty())
+			e.append(") (aspecto " + d.aspecto);
+		if (!d.aspecto.isEmpty())
+			e.append(") (inflamacion " + d.inflamacion);
+		if (!d.ritmo_evacuatorio.isEmpty())
+			e.append(") (ritmo_evacuatorio " + d.ritmo_evacuatorio);
+		e.append(") )");
+
+		env.assertString(e.toString());
 	}
-	
+
 	private void agregarAntecedentesPaciente(ArrayList<AntecedentesPaciente> a) {
-		if(a.size() > 0) {
-			String entrada = "(antecedente_paciente (enfermedad";
-			
-			for(int i = 0; i < a.size(); i++) {
+		if (a.size() > 0) {
+			StringBuilder entrada = new StringBuilder("(antecedente_paciente (enfermedad");
+
+			for (int i = 0; i < a.size(); i++) {
 				AntecedentesPaciente p = a.get(i);
-				entrada = entrada + " " + p.enfermedad;
+				entrada.append(" " + p.enfermedad);
 			}
-			
-			entrada = entrada + ") (id_paciente " + a.get(0).id_paciente + ") )";
-			
-			System.out.println("[Ante Pac]: " + entrada);
-			
-			env.assertString(entrada);
+
+			entrada.append(") (id_paciente " + a.get(0).id_paciente + ") )");
+
+			env.assertString(entrada.toString());
 		}
 	}
-	
+
 	private void agregarAntecedentesFamiliares(ArrayList<AntecedentesFamiliares> a) {
-		if(a.size() > 0) {
-			String entrada = "(antecedente_familiar (enfermedad";
-			
-			for(int i = 0; i < a.size(); i++) {
+		if (a.size() > 0) {
+			StringBuilder entrada = new StringBuilder("(antecedente_familiar (enfermedad");
+
+			for (int i = 0; i < a.size(); i++) {
 				AntecedentesFamiliares p = a.get(i);
-				entrada = entrada + " " + p.enfermedad;
+				entrada.append(" " + p.enfermedad);
 			}
-			
-			entrada = entrada + ") (id_paciente " + a.get(0).id_paciente + ") )";
-			
-			System.out.println(entrada);
-			
-			env.assertString("[Ante Fam]: " + entrada);
+
+			entrada.append(") (id_paciente " + a.get(0).id_paciente + ") )");
+
+			env.assertString(entrada.toString());
 		}
 	}
-	
+
 	private void agregarEstudio(Estudio e) {
-		if(e == null)
+		if (e == null)
 			return;
-		
-		String entrada = "(estudio (id_estudio " + e.id_estudio
-				+ ") (id_paciente " + e.id_paciente
-				+ ") (estado " + e.estado
-				+ ") (tipo_estudio " + e.tipo_analisis
-				+ ") (resultado " + e.resultado + ") )";
-		
-		System.out.println("[Estudio]: " + entrada);
-		
+
+		String entrada = "(estudio (id_estudio " + e.id_estudio + ") (id_paciente " + e.id_paciente + ") (estado "
+				+ e.estado + ") (tipo_estudio " + e.tipo_analisis + ") (resultado " + e.resultado + ") )";
+
 		env.assertString(entrada);
 	}
-	
+
 	private void agregarLaboratorio(Laboratorio l) {
-		if(l == null)
+		if (l == null)
 			return;
-		
-		String entrada = "(laboratorio (id_laboratorio " + l.id_laboratorio
-				+ ") (id_paciente " + l.id_paciente
-				+ ") (ERS " + l.ERS
-				+ ") (PCR " + l.PCR + ") )";
-		
-		System.out.println("[Laboratorio]: " + entrada);
-		
+
+		String entrada = "(laboratorio (id_laboratorio " + l.id_laboratorio + ") (id_paciente " + l.id_paciente
+				+ ") (ERS " + l.ERS + ") (PCR " + l.PCR + ") )";
+
 		env.assertString(entrada);
 	}
-	
+
 	private void agregarDiagnostico(int id_paciente, int id_diagnostico, Date fecha) {
-		// ( diagnostico  ( id_paciente 104 ) ( id_diagnostico 5 ) ( fecha 23-6-2018 ) )
-		String entrada = "(diagnostico (id_paciente " + id_paciente
-				+ ") (id_diagnostico " + id_diagnostico
+		String entrada = "(diagnostico (id_paciente " + id_paciente + ") (id_diagnostico " + id_diagnostico
 				+ ") (fecha " + fecha + ") )";
-		
-		System.out.println("[Diagnostico]: " + entrada);
-		
+
 		env.assertString(entrada);
 	}
-	
+
 	public void limpiar() {
 		env.reset();
 	}
-	
+
 }
